f4180db078425de11c9e897a1d801278f7742ddd
Ilona Oprea <35139973+ilo-op@users.noreply.github.com>
2019-07-09 10:43:34 +0300

    Add files via upload

---
+++ demoli/src/main/java/com/example/demoli/DemooliApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemooliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemooliApplication.class, args);
+	}
+
+}
---
+++ demoli/src/test/java/com/example/demoli/DemooliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemooliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
